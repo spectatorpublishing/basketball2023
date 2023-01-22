6ae952ef3c244bc9991a581fb65fa061a1f1bc66
Maya Srikanth <ms6198@columbia.edu>
2023-01-21 21:26:42 -0500

    Update Credits.js

--- src/containers/Credits.js
+++ src/containers/Credits.js
@@ -87,7 +87,7 @@ const SectAndNames = styled.div`
     width: 70%;
     height: 60%;
     @media only screen and (max-width: 768px){
-        margin-left: 1.5rem;
+        margin-left: 1rem;
         display: block;
         flex-direction: column;
         flex-wrap: nowrap;
